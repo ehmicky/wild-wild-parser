b50f151251b23ee5b1be770f7a96a3689cf1861d
ehmicky <ehmicky@gmail.com>
2022-03-20 17:33:55 +0100

    Split file

--- test/path.js
+++ test/path.js
@@ -1,11 +1,6 @@
 import test from 'ava'
 import { each } from 'test-each'
-import {
-  parsePath,
-  serializePath,
-  normalizePath,
-  isSamePath,
-} from 'wild-wild-parser'
+import { parsePath, serializePath, normalizePath } from 'wild-wild-parser'
 
 each(
   [
@@ -76,31 +71,3 @@ each([[-1], '-1'], ({ title }, arg) => {
     t.throws(normalizePath.bind(undefined, arg))
   })
 })
-
-each(
-  [
-    { queryA: [], queryB: [], output: true },
-    { queryA: [], queryB: ['a'], output: false },
-    { queryA: ['a'], queryB: 'a', output: true },
-    { queryA: 'a', queryB: 'a.b', output: false },
-  ],
-  ({ title }, { queryA, queryB, output }) => {
-    test(`isSamePath() output | ${title}`, (t) => {
-      t.is(isSamePath(queryA, queryB), output)
-    })
-  },
-)
-
-each(
-  [
-    { queryA: true, queryB: [] },
-    { queryA: 'a b', queryB: 'a' },
-    { queryA: '0:', queryB: 'a' },
-    { queryA: -0, queryB: 0 },
-  ],
-  ({ title }, { queryA, queryB }) => {
-    test(`isSamePath() validates input | ${title}`, (t) => {
-      t.throws(isSamePath.bind(undefined, queryA, queryB))
-    })
-  },
-)
