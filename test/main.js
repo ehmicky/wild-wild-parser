f0bbfe75ed97713bd799f5686d16b6eb0f62fad9
ehmicky <ehmicky@gmail.com>
2022-03-20 15:51:07 +0100

    Add more tests

--- test/main.js
+++ test/main.js
@@ -2,6 +2,15 @@ import test from 'ava'
 import { each } from 'test-each'
 import { parseQuery } from 'wild-wild-parser'
 
+each(
+  [{ queryString: 'prop', output: [['prop']] }],
+  ({ title }, { queryString, output }) => {
+    test(`parseQuery() output | ${title}`, (t) => {
+      t.deepEqual(parseQuery(queryString), output)
+    })
+  },
+)
+
 each([[], [[]], ['prop'], [['prop']], '', ' '], ({ title }, arg) => {
   test(`parseQuery() validates input | ${title}`, (t) => {
     t.throws(parseQuery.bind(undefined, arg))
