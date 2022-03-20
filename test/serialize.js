b0863fb683490a762cb41be88068e8d6c5494d2d
ehmicky <ehmicky@gmail.com>
2022-03-20 15:56:54 +0100

    Refactoring

--- test/serialize.js
+++ test/serialize.js
@@ -5,7 +5,7 @@ import { serializeQuery } from 'wild-wild-parser'
 each(
   [
     // Dots delimiters
-    { queryArrays: [['a', 'b']], output: 'a.b' },
+    { queryArrays: ['a', 'b'], output: 'a.b' },
 
     // Space delimiters
     { queryArrays: [['a'], ['b']], output: 'a b' },
@@ -13,65 +13,62 @@ each(
     // Dots non-delimiters
     { queryArrays: [], output: '.' },
     { queryArrays: [[]], output: '.' },
-    { queryArrays: [['']], output: '..' },
-    { queryArrays: [['', '']], output: '...' },
-    { queryArrays: [['a', '']], output: 'a.' },
-    { queryArrays: [['', 'a']], output: '..a' },
+    { queryArrays: [''], output: '..' },
+    { queryArrays: ['', ''], output: '...' },
+    { queryArrays: ['a', ''], output: 'a.' },
+    { queryArrays: ['', 'a'], output: '..a' },
 
     // Escape characters
-    { queryArrays: [['0']], output: '\\0' },
-    { queryArrays: [['*']], output: '\\*' },
-    { queryArrays: [['**']], output: '\\**' },
-    { queryArrays: [[':']], output: '\\:' },
-    { queryArrays: [['/a/']], output: '\\/a/' },
-    { queryArrays: [['.']], output: '\\.' },
-    { queryArrays: [[' ']], output: '\\ ' },
-    { queryArrays: [['\\']], output: '\\\\' },
+    { queryArrays: ['0'], output: '\\0' },
+    { queryArrays: ['*'], output: '\\*' },
+    { queryArrays: ['**'], output: '\\**' },
+    { queryArrays: [':'], output: '\\:' },
+    { queryArrays: ['/a/'], output: '\\/a/' },
+    { queryArrays: ['.'], output: '\\.' },
+    { queryArrays: [' '], output: '\\ ' },
+    { queryArrays: ['\\'], output: '\\\\' },
 
     // Prop tokens
-    { queryArrays: [['a']], output: 'a' },
+    { queryArrays: ['a'], output: 'a' },
 
     // Index tokens
-    { queryArrays: [[1]], output: '1' },
-    { queryArrays: [[0]], output: '0' },
-    { queryArrays: [[-1]], output: '-1' },
-    { queryArrays: [[-0]], output: '-0' },
-    { queryArrays: [[0, 1]], output: '0.1' },
-    { queryArrays: [['1 ']], output: '1\\ ' },
-    { queryArrays: [[' 1']], output: '\\ 1' },
-    { queryArrays: [['1n']], output: '1n' },
-    { queryArrays: [['1e3']], output: '1e3' },
-    { queryArrays: [['Infinity']], output: 'Infinity' },
-    { queryArrays: [['NaN']], output: 'NaN' },
+    { queryArrays: [1], output: '1' },
+    { queryArrays: [0], output: '0' },
+    { queryArrays: [-1], output: '-1' },
+    { queryArrays: [-0], output: '-0' },
+    { queryArrays: [0, 1], output: '0.1' },
+    { queryArrays: ['1 '], output: '1\\ ' },
+    { queryArrays: [' 1'], output: '\\ 1' },
+    { queryArrays: ['1n'], output: '1n' },
+    { queryArrays: ['1e3'], output: '1e3' },
+    { queryArrays: ['Infinity'], output: 'Infinity' },
+    { queryArrays: ['NaN'], output: 'NaN' },
 
     // Slice tokens
-    { queryArrays: [[{ type: 'slice', from: 0 }]], output: '0:' },
-    { queryArrays: [[{ type: 'slice', from: 0, to: -0 }]], output: '0:' },
-    {
-      queryArrays: [[{ type: 'slice', from: 0, to: undefined }]],
-      output: '0:',
-    },
-    { queryArrays: [[{ type: 'slice' }]], output: '0:' },
-    { queryArrays: [[{ type: 'slice', from: 1, to: 1 }]], output: '1:1' },
-    { queryArrays: [[{ type: 'slice', from: -1, to: -1 }]], output: '-1:-1' },
-    { queryArrays: [[{ type: 'slice', other: true }]], output: '0:' },
+    { queryArrays: [{ type: 'slice', from: 0 }], output: '0:' },
+    { queryArrays: [{ type: 'slice', from: 0, to: -0 }], output: '0:' },
+    { queryArrays: [{ type: 'slice', from: 0, to: undefined }], output: '0:' },
+    { queryArrays: [{ type: 'slice' }], output: '0:' },
+    { queryArrays: [{ type: 'slice', from: 1, to: 1 }], output: '1:1' },
+    { queryArrays: [{ type: 'slice', from: -1, to: -1 }], output: '-1:-1' },
+    { queryArrays: [{ type: 'slice', other: true }], output: '0:' },
 
     // RegExp tokens
     // eslint-disable-next-line require-unicode-regexp
-    { queryArrays: [[/a/]], output: '/a/' },
-    { queryArrays: [[/a/u]], output: '/a/u' },
-    { queryArrays: [[/a.b/u]], output: '/a\\.b/u' },
-    { queryArrays: [[/a b/u]], output: '/a\\ b/u' },
-    { queryArrays: [[/a\b/u]], output: '/a\\\\b/u' },
-    { queryArrays: [[/a\/b/u]], output: '/a\\\\/b/u' },
+    { queryArrays: [/a/], output: '/a/' },
+    { queryArrays: [/a/u], output: '/a/u' },
+    { queryArrays: [/a.b/u], output: '/a\\.b/u' },
+    { queryArrays: [/a b/u], output: '/a\\ b/u' },
+    { queryArrays: [/a\b/u], output: '/a\\\\b/u' },
+    { queryArrays: [/a\/b/u], output: '/a\\\\/b/u' },
 
     // any tokens
-    { queryArrays: [[{ type: 'any' }]], output: '*' },
-    { queryArrays: [[{ type: 'any', other: true }]], output: '*' },
+    { queryArrays: [{ type: 'any' }], output: '*' },
+    { queryArrays: [{ type: 'any', other: true }], output: '*' },
 
     // anyDeep tokens
-    { queryArrays: [[{ type: 'anyDeep' }]], output: '**' },
-    { queryArrays: [[{ type: 'anyDeep', other: true }]], output: '**' },
+    { queryArrays: [{ type: 'anyDeep' }], output: '**' },
+    { queryArrays: [{ type: 'anyDeep', other: true }], output: '**' },
   ],
   ({ title }, { queryArrays, output }) => {
     test(`serializeQuery() output | ${title}`, (t) => {
