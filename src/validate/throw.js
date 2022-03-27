6c6c26784cb3c8a1142ed238d865ff11449d5400
ehmicky <ehmicky@gmail.com>
2022-03-27 17:52:38 +0200

    Do not use Node.js utilities

--- src/validate/throw.js
+++ src/validate/throw.js
@@ -1,11 +1,9 @@
-import { inspect } from 'util'
-
 // Throw an error when a token is invalid
 export const throwTokenError = function (queryArray, token, message) {
-  throwQueryError(queryArray, `Invalid token: ${inspect(token)}\n${message}`)
+  throwQueryError(queryArray, `Invalid token: ${token}\n${message}`)
 }
 
 // Throw an error when a query is invalid
 export const throwQueryError = function (query, message) {
-  throw new Error(`Invalid query: ${inspect(query)}\n${message}`)
+  throw new Error(`Invalid query: ${query}\n${message}`)
 }
