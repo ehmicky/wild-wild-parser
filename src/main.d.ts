79a43bd745efcdc9c7180f73fb3d9f6f6a0023ef
ehmicky <ehmicky@gmail.com>
2022-03-27 19:48:55 +0200

    Add TypeScript types

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,1 +1,54 @@
-export default function example(): void
+type RegExpToken = RegExp
+type IndexToken = number
+type PropToken = string
+interface AnyDeepToken {
+  type: 'anyDeep'
+}
+interface AnyToken {
+  type: 'any'
+}
+interface SliceToken {
+  type: 'slice'
+  from?: IndexToken
+  to?: IndexToken
+}
+
+export type QueryToken = Readonly<
+  AnyDeepToken | AnyToken | RegExpToken | SliceToken | IndexToken | PropToken
+>
+export type PathToken = Readonly<IndexToken | PropToken>
+
+export type TokenType =
+  | 'unknown'
+  | 'anyDeep'
+  | 'any'
+  | 'regExp'
+  | 'slice'
+  | 'index'
+  | 'prop'
+
+export type PathString = string
+export type QueryString = string
+
+export type PathArray = PathToken[]
+export type QueryArray = QueryToken[]
+
+export type Path = PathString | PathArray
+export type Query = QueryString | QueryArray
+
+export function getTokenType(token: QueryToken): TokenType
+
+export function isSameToken(
+  firstToken: QueryToken,
+  secondToken: QueryToken,
+): boolean
+export function isSamePath(firstPath: Path, secondPath: Path): boolean
+export function isParentPath(parentPath: Path, childPath: Path): boolean
+export function isSameQuery(firstQuery: Query, secondQuery: Query): boolean
+
+export function parsePath(pathString: PathString): PathArray
+export function parseQuery(queryString: QueryString): QueryArray
+export function serializePath(pathArray: PathArray): PathString
+export function serializeQuery(queryArray: QueryArray): QueryString
+export function normalizePath(path: Path): PathArray
+export function normalizeQuery(query: Query): QueryArray
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,4 +1,74 @@
-import wildWildParser from 'wild-wild-parser'
+import {
+  getTokenType,
+  isSameToken,
+  isSamePath,
+  isParentPath,
+  isSameQuery,
+  parsePath,
+  parseQuery,
+  serializePath,
+  serializeQuery,
+  normalizePath,
+  normalizeQuery,
+  QueryToken,
+  PathToken,
+  TokenType,
+  PathString,
+  QueryString,
+  PathArray,
+  QueryArray,
+  Path,
+  Query,
+} from 'wild-wild-parser'
 import { expectType, expectError } from 'tsd'
 
-await wildWildParser()
+const pathToken: PathToken = 'prop'
+const queryToken: QueryToken = { type: 'any' }
+const pathArray: PathArray = ['prop']
+const queryArray: QueryArray = [{ type: 'any' }]
+const pathString: PathString = 'prop'
+const queryString: QueryString = '*'
+const tokenType: TokenType = 'index'
+const pathA: Path = ['prop']
+const pathB: Path = 'prop'
+const queryA: Query = [{ type: 'any' }]
+const queryB: Query = '*'
+
+expectType<TokenType>(getTokenType('prop'))
+expectError(getTokenType({ type: 'other' }))
+expectError(getTokenType({ type: 'anyDeep', other: true }))
+getTokenType({ type: 'anyDeep' })
+expectError(getTokenType({ type: 'any', other: true }))
+getTokenType({ type: 'any' })
+getTokenType(/regexp/)
+getTokenType('prop')
+getTokenType(0)
+getTokenType(-0)
+getTokenType({ type: 'slice' })
+getTokenType({ type: 'slice', from: undefined, to: undefined })
+getTokenType({ type: 'slice', from: 0, to: 1 })
+expectError(getTokenType({ type: 'slice', other: true }))
+
+expectType<boolean>(isSameToken(0, '0'))
+expectError(isSameToken(0, true))
+expectType<boolean>(isSamePath([0], '0'))
+expectError(isSamePath([0], true))
+expectType<boolean>(isParentPath([0], '0'))
+expectError(isParentPath([0], true))
+expectType<boolean>(isSameQuery([{ type: 'any' }], '*'))
+expectError(isSameQuery([{ type: 'any' }], true))
+
+expectType<PathArray>(parsePath('prop'))
+expectError(parsePath(true))
+expectType<QueryArray>(parseQuery('*'))
+expectError(parseQuery(true))
+expectType<PathString>(serializePath(['prop']))
+expectError(serializePath(true))
+expectType<QueryString>(serializeQuery([{ type: 'any' }]))
+expectError(serializeQuery(true))
+expectType<PathArray>(normalizePath('prop'))
+normalizePath(['prop'])
+expectError(normalizePath(true))
+expectType<QueryArray>(normalizeQuery('*'))
+normalizeQuery([{ type: 'any' }])
+expectError(normalizeQuery(true))
