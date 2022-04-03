8a295550dee17c9f8a20ea4a16ef9b9b4c1e2446
ehmicky <ehmicky@gmail.com>
2022-04-03 11:52:15 +0200

    More memoization

--- src/tokens/other.js
+++ src/tokens/other.js
@@ -1,3 +1,5 @@
+import moize from 'moize'
+
 import { ANY_TOKEN, ANY_DEEP_TOKEN } from './any.js'
 import { INDEX_TOKEN } from './indices.js'
 import { REGEXP_TOKEN } from './regexp.js'
@@ -5,11 +7,15 @@ import { SLICE_TOKEN } from './slice.js'
 
 // Retrieve the type of a given token serialized string, except the default
 // one (property name string)
-export const getOtherStringTokenType = function (chars) {
+const mGetOtherStringTokenType = function (chars) {
   return OTHER_STRING_TOKEN_TYPES.find((tokenType) =>
     tokenType.testString(chars),
   )
 }
+
+export const getOtherStringTokenType = moize(mGetOtherStringTokenType, {
+  maxSize: 1e3,
+})
 
 // Order is significant as they are tested serially.
 // It is optimized for common use cases and performance.
